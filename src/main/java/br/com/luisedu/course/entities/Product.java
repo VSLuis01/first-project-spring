0fdb3c9f3fe0ba5f0f4036364edc667b0bee8919
Luis Eduardo <vsluiseduardo01@gmail.com>
2024-11-04 23:13:11 -0400

    Many-to-many association with JoinTable.

--- src/main/java/br/com/luisedu/course/entities/Product.java
+++ src/main/java/br/com/luisedu/course/entities/Product.java
@@ -24,7 +24,12 @@ public class Product implements Serializable {
     private Double price;
     private String imageUrl;
 
-    @Transient
+    @ManyToMany // Mapeamento para transformar em uma tabela de associação
+    @JoinTable(
+            name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
     private Set<Category> categories = new HashSet<>();
 
     public Product() {}
