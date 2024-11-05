e6cf46d644e373c5009292a237d853681af00c9f
Luis Eduardo <vsluiseduardo01@gmail.com>
2024-11-04 20:29:27 -0400

    User entity and resource.

---
+++ src/main/java/br/com/luisedu/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package br.com.luisedu.course.resources;
+
+import br.com.luisedu.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Luis", "luis@gmail.com", "9999", "1234");
+        return ResponseEntity.ok().body(u);
+    }
+}
